2bb514cd240f42f86ccd14e7a4fb12e9eb8a2323
variablevanaa <bhavanakalam04@gmail.com>
2026-01-28 17:54:28 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,11 @@
+num1 = int(input("please enter first number:"))
+num2 = int(input("please enter the second number:"))
+
+addition = num1+num2
+print("the addition of these two numbers is", addition)
+substraction = num1-num2
+print("the substraction of these two numbers is", substraction)
+multiply = num1*num2
+print("the multiplication of these two numbers is", multiply)
+division = num1/num2
+print("the division of these two numbers is", division)
---
+++ Task 2.py
@@ -0,0 +1,4 @@
+first_name = input("What is your first name?")
+last_name = input("What is your last name?")
+
+print("hello", first_name,last_name,"welcome to the python world!!")
